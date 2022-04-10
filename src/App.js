b00ac35fee212a7fd9b901b40961f7c2beff562f
ChazzitUP <hamzarezgui.education@gmail.com>
2022-04-10 17:06:03 +0100

    added header component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 import {Route} from 'react-router-dom';
+import { Switch } from "react-router-dom";
 
 
 
@@ -34,6 +36,8 @@ const WomenPage = ()=> (
 function App() {
   return (
     <div>
+      <Header/>
+      <Switch>
 
       
         <Route exact path="/" component={HomePage}/>
@@ -42,7 +46,7 @@ function App() {
         <Route exact path="/sneakers" component={SneakersPage}/>
         <Route exact path="/men" component={MenPage}/>
         <Route exact path="/women" component={WomenPage}/>
-    
+      </Switch>
      
     </div>
   );
